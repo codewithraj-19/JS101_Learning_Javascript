e7431aee71f92641d526f298ae9bfbf349bf433f
SHiiKARii <rituraj282@gmail.com>
2023-10-23 14:24:32 +0530

    added iw1.js

---
+++ iw1.js
@@ -0,0 +1,8 @@
+// problem 1: Print the numbers from the given starting point till ending point (including both start and end)
+
+let num=1;
+while(num<=5)
+  {
+    console.log(num);
+    num++;
+  }
---
+++ iw2.js
@@ -0,0 +1,14 @@
+// Problem 2: Print the odd numbers from 0 till the given limit
+
+let num=0
+while(num<=20)
+  {
+    if(num%2==1)
+    {
+    console.log(num);  
+      n++;
+      
+    }
+    
+    
+  }
